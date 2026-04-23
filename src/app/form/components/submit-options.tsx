1f6cc22d886bb099546f34ad2a0fdff46119de3a
Sean <seancyrill@gmail.com>
2026-04-23 18:19:37 +0800

    reenable submit

--- src/app/form/components/submit-options.tsx
+++ src/app/form/components/submit-options.tsx
@@ -74,10 +74,10 @@ export function SubmitModal({ open, onClose }: SubmitModalProps) {
           function: () => {
             router.push("/")
             clearStatus()
+            resetApplication()
           },
         },
       })
-      // resetApplication()
     } else {
       console.error(data.error)
     }
